c5e66d0237b5b901871dbf0876c12e70708aa89f
Hayden Whiteman <haydenwhiteman@gmail.com>
2024-12-31 11:39:49 +1300

    Update variant.go
    Allow setting compare at price to null

--- variant.go
+++ variant.go
@@ -59,7 +59,7 @@ type Variant struct {
 	Grams                int                    `json:"grams,omitempty"`
 	InventoryPolicy      VariantInventoryPolicy `json:"inventory_policy,omitempty"`
 	Price                *decimal.Decimal       `json:"price,omitempty"`
-	CompareAtPrice       *decimal.Decimal       `json:"compare_at_price,omitempty"`
+	CompareAtPrice       *decimal.Decimal       `json:"compare_at_price"`
 	FulfillmentService   string                 `json:"fulfillment_service,omitempty"`
 	InventoryManagement  string                 `json:"inventory_management,omitempty"`
 	InventoryItemId      uint64                 `json:"inventory_item_id,omitempty"`
